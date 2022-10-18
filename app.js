f35db34508f3a4e7e64515434f67743f1bf6a8d5
Aswin <aswindilip5@gmail.com>
2022-10-18 07:33:33 +0530

    mongodb connected

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const mongoose = require('mongoose')
+//mongodb connections 
+mongoose.connect("mongodb://localhost:27017/ToDoListDB")
+//
+const itemSchema = {
+    name:String
+}
+
+const Item = mongoose.model('item',itemSchema);
+
 
 //seting view engine
 app.set('views',path.join(__dirname,'views'))
