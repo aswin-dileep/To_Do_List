df33b2aeafc881af93b11ece86449415f8cb3020
Aswin <aswindilip5@gmail.com>
2022-10-18 21:01:03 +0530

    insertion part completed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+
 //mongodb connections 
 mongoose.connect("mongodb://localhost:27017/ToDoListDB")
 //
@@ -16,11 +18,25 @@ const Item = mongoose.model('item',itemSchema);
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs');
 
+//parser connection
+app.use(bodyParser.urlencoded({extended:true}));
+
 //setting public folder
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    res.render('index')
+    Item.find({},async (err,result)=>{
+        if(!err){
+            res.render('index',{Data:result})
+        }
+    })
+   
+})
+app.post("/",(req,res)=>{
+    const item = req.body.item;
+    const data = new Item({name:item})
+    data.save();
+    res.redirect('/')
 })
 
 app.listen('3000',(req,res)=>{
